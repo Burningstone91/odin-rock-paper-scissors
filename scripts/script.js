3aa28347ae9eb678ef41ea5078930ffc9bacbb04
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-02-28 16:46:08 +0100

    Add function to calculate who wins the game

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,18 @@ function getComputerChoice() {
   const random = Math.floor(Math.random() * 3)
   return choices[random]
 }
+
+let playerSelection = "rock"
+let computerSelection = getComputerChoice();
+
+function calculateWinner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "tie";
+  } else if ((playerSelection == "rock" && computerSelection == "scissor") ||
+      (playerSelection == "paper" && computerSelection == "rock") ||
+      (playerSelection == "scissor" && computerSelection == "paper")) {
+    return "player";
+  } else {
+    return "computer";
+  }
+}
